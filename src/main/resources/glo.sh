8f9f96f3942071b54cffa24f3a4a5d552c8452ad
souled <souled3000@163.com>
2017-12-06 16:16:45 +0800

    a

---
+++ src/main/resources/glo.sh
@@ -0,0 +1,1 @@
+storm jar xaviera-0.0.2.jar ind.storm.glovanna.Glo 192.168.2.177:2181/kafka 192.168.2.177:9092 glo 1
--- src/test/java/ind/storm/xaviera/AppTest.java
+++ src/test/java/ind/storm/xaviera/AppTest.java
@@ -1,5 +1,8 @@
 package ind.storm.xaviera;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +10,45 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+	
+	public void testTreeMap() {
+		System.out.println(System.currentTimeMillis());
+		Map<Long,String> m = new TreeMap<Long,String>();
+		m.put(100L, "a");
+		m.put(200L, "b");
+		for(Map.Entry<Long, String> o:m.entrySet()) {
+			System.out.println(o.getValue());
+		}
+		int step = 30;
+		int area = 1440/30;
+		for (int i = 0; i < area; i++) {
+			for (int j = i * step; j < i * step + step; j++) {
+				System.out.println(j);
+			}
+		}
+	}
 }
