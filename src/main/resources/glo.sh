e6b21195cb3e3cba18f4e09d9f4b4867da019041
souled <souled3000@163.com>
2017-12-07 12:25:33 +0800

    1

--- src/main/resources/glo.sh
+++ src/main/resources/glo.sh
@@ -1,1 +1,1 @@
-storm jar xaviera-0.0.2.jar ind.storm.glovanna.Glo 192.168.2.177:2181/kafka 192.168.2.177:9092 glo 1
+storm jar xaviera-0.0.2.jar ind.storm.glovanna.Glo localhost:2181/kafka localhost:9092 glo 1
--- src/main/resources/xaviera.sh
+++ src/main/resources/xaviera.sh
@@ -1,1 +1,1 @@
-./storm jar ./xaviera-0.0.1.jar ind.storm.xaviera.Xaviera 10.165.125.124:2181/kafka 10.165.125.124:9092 joseph 1
+./storm jar ./xaviera-0.0.1.jar ind.storm.xaviera.Xaviera localhost:2181/kafka localhost:9092 joseph 1
